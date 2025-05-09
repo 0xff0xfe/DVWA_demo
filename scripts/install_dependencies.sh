4e60d6f6eea1e6970f816fcf336a1256590e408f
0xff0xfe <0xff0xfe@gmail.com>
2025-05-09 22:21:07 +0800

    update 'comment out'

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,4 @@
 # Install Apache, PHP, and MySQL for DVWA
 apt-get update
 apt-get install -y apache2 php mysql-server php-mysqli git
-chown -R www-data:www-data /var/www/dvwa
+#chown -R www-data:www-data /var/www/dvwa
